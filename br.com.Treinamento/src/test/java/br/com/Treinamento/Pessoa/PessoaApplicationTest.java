6397ff3d9916551cfe021861346d19fa0904ab6c
Rafael <rlgonzalez@apoioprodesp.sp.gov.br>
2023-01-05 11:20:29 -0300

    Alteração nos Testes

--- br.com.Treinamento/src/test/java/br/com/Treinamento/Pessoa/PessoaApplicationTest.java
+++ br.com.Treinamento/src/test/java/br/com/Treinamento/Pessoa/PessoaApplicationTest.java
@@ -1,13 +1,8 @@
 package br.com.Treinamento.Pessoa;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@ActiveProfiles("test")
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class PessoaApplicationTest {
 	
